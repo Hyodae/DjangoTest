4486e7da3612db91b9b9f1906918da30c926a3d5
Ubuntu <ubuntu@ip-172-31-1-71.ap-northeast-2.compute.internal>
2019-02-07 09:02:08 +0900

    add home

---
+++ home/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+from matplotlib import pylab
+from pylab import *
+from PIL import Image
+import PIL.Image
+
+import io
+
+
+# Create your views here.
+
+def showimage(request):
+    # Construct the graph
+    t = arange(0.0, 2.0, 0.01)
+    s = sin(2*pi*t)
+    plot(t, s, linewidth=1.0)
+
+    xlabel('time (s)')
+    ylabel('voltage (mV)')
+    title('Simple Sine Graph')
+    grid(True)
+
+    # Store image in a string buffer
+    buffer = io.BytesIO()
+    canvas = pylab.get_current_fig_manager().canvas
+    canvas.draw()
+    pilImage = PIL.Image.frombytes("RGB", canvas.get_width_height(), canvas.tostring_rgb())
+    pilImage.save(buffer, "PNG")
+    pylab.close()
+
+    # Send buffer in a http response the the browser with the mime type image/png set
+    return HttpResponse(buffer.getvalue(), content_type="image/png")
